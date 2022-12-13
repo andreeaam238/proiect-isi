a2b6217cedad8686be7399f426329b4ac42456a2
andreeaam238 <blagaandreea238@yahoo.com>
2022-12-13 18:06:10 +0200

    Setup for Bucharest map

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { setDefaultOptions, loadModules } from 'esri-loader';
+
+import esri = __esri; // Esri TypeScript Types
 
 @Component({
   selector: 'app-map',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  @ViewChild('map') mapEl: ElementRef;
+  mapView: any = null;
 
-  constructor() { }
+  constructor(public platform: Platform) {}
 
-  ngOnInit() {
+  async getGeo() {
+    await this.platform.ready();
+
+    const [Map, MapView, Compass]: any = await loadModules([
+      'esri/Map',
+      'esri/views/MapView',
+      'esri/widgets/Compass',
+    ]).catch((err) => {
+      console.error('ArcGIS: ', err);
+    });
+
+    let map = new Map({
+      basemap: 'hybrid',
+    });
+
+    let mapView = new MapView({
+      container: this.mapEl.nativeElement,
+      center: [26.096306, 44.439663],
+      zoom: 12,
+      map: map,
+    });
+
+    let compassWidget = new Compass({
+      view: mapView,
+    });
+
+    mapView.ui.add(compassWidget, 'top-left');
   }
 
+  ngOnInit() {
+    this.getGeo();
+  }
 }
