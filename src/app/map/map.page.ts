a72a051dbeb3acb74037055e7dd74c6cee4c6a6c
andreeaam238 <blagaandreea238@yahoo.com>
2022-12-20 18:25:04 +0200

    Add Feature Layer for Bike Trails & Popups

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { loadModules } from 'esri-loader';
+import { loadModules, setDefaultOptions } from 'esri-loader';
 
 import esri = __esri; // Esri TypeScript Types
 
@@ -17,19 +17,29 @@ export class MapPage implements OnInit {
 
   async getGeo() {
     await this.platform.ready();
+    setDefaultOptions({ css: true });
 
-    const [esriConfig, Map, MapView, Compass, Locate, Track, Graphic]: any =
-      await loadModules([
-        'esri/config',
-        'esri/Map',
-        'esri/views/MapView',
-        'esri/widgets/Compass',
-        'esri/widgets/Locate',
-        'esri/widgets/Track',
-        'esri/Graphic',
-      ]).catch((err) => {
-        console.error('ArcGIS: ', err);
-      });
+    const [
+      esriConfig,
+      Map,
+      MapView,
+      Compass,
+      Locate,
+      Track,
+      Graphic,
+      FeatureLayer,
+    ]: any = await loadModules([
+      'esri/config',
+      'esri/Map',
+      'esri/views/MapView',
+      'esri/widgets/Compass',
+      'esri/widgets/Locate',
+      'esri/widgets/Track',
+      'esri/Graphic',
+      'esri/layers/FeatureLayer',
+    ]).catch((err) => {
+      console.error('ArcGIS: ', err);
+    });
 
     esriConfig.apiKey =
       'AAPK96c3dc2e11734f96852b9f83319128a7AwtXTcRvExfL9-yYgVLNKW5ONYv3yLXUNbel0FxQySEL4FyIbt1Fuw7njdOAaljf';
@@ -37,6 +47,63 @@ export class MapPage implements OnInit {
     let map = new Map({
       basemap: 'arcgis-navigation',
     });
+
+    const popupTrails = {
+      title: '{Name}',
+      content: [
+        {
+          type: 'fields',
+          fieldInfos: [
+            {
+              fieldName: 'Length',
+              label: 'Length',
+              isEditable: false,
+              tooltip: '',
+              visible: true,
+              format: null,
+              stringFieldOption: 'text-box',
+            },
+          ],
+        },
+      ],
+    };
+
+    const bikeTrailsLineRenderer = {
+      type: 'simple',
+      symbol: {
+        color: '#FF7518',
+        type: 'simple-line',
+        style: 'solid',
+        width: '5px',
+        height: '5px',
+      },
+    };
+
+    const bikeTrailsLineFeatureLayer: __esri.FeatureLayer = new FeatureLayer({
+      url: 'https://services7.arcgis.com/BPmbOobL1X8Rtqka/arcgis/rest/services/piste_de_biciclete_bucuresti/FeatureServer/0',
+      renderer: bikeTrailsLineRenderer,
+      outFields: ['Name', 'Length'],
+      popupTemplate: popupTrails,
+    });
+
+    map.add(bikeTrailsLineFeatureLayer);
+
+    const trailheadsRenderer = {
+      type: 'simple',
+      symbol: {
+        type: 'picture-marker',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgIRKgEPoZRTpSv_WUnesz_cOKL_K129o81w&usqp=CAU',
+        width: '13px',
+        height: '13px',
+      },
+    };
+
+    const bikeTrailsFeatureLayer: __esri.FeatureLayer = new FeatureLayer({
+      url: 'https://services7.arcgis.com/BPmbOobL1X8Rtqka/arcgis/rest/services/piste_de_biciclete_bucuresti/FeatureServer/0',
+      renderer: trailheadsRenderer,
+    });
+
+    map.add(bikeTrailsFeatureLayer);
 
     let mapView = new MapView({
       container: this.mapEl.nativeElement,
